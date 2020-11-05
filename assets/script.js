71c00be235d1fd3d8dd244186066522a28c774d5
Peter Phenow <peter.phenow@gmail.com>
2020-11-05 10:32:29 -0600

    completed placing info from 1st API call into HTML and 2nd API call started and working

--- assets/script.js
+++ assets/script.js
@@ -20,9 +20,10 @@ searchBtn.on("click", function (event) {
   }).then(function (response) {
     //log the resulting object
     console.log(response);
-    //create lat and lon  and icon variables
+    //create lat and lon variables for another API call
     let lat = response.coord.lat;
     let lon = response.coord.lon;
+    //create icon variable
     let weatherIcon = response.weather[0].icon;
     //create temperature variable and convert to fahrenheit
     let tempF = ((response.main.temp - 273.15) * 1.8 + 32).toFixed(1);
@@ -32,8 +33,34 @@ searchBtn.on("click", function (event) {
     console.log(lat, lon, weatherIcon, tempF, humidity, wind);
 
     // Create CODE HERE to transfer content to HTML
+    $("#city").text(citySearch);
     $("#temp").text("Temperature: " + tempF);
     $("#humidity").text("Humidity: " + humidity + "%");
     $("#wind-speed").text("Wind Speed: " + wind + " mph");
+    //create and set current date
+    let today = new Date().toLocaleDateString();
+    $("#date").text(today);
+    //set weather icon
+    $("#weather-icon").attr("src", "http://openweathermap.org/img/wn/" + weatherIcon + ".png");
+
+    //set url for 2nd API call
+    let queryURL2 =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&exclude=hourly,minutely&appid=" +
+      APIKey;
+
+    $.ajax({
+      url: queryURL2,
+      method: "GET",
+    }).then(function (response2) {
+      console.log(response2);
+      //create and set UV Index variable
+      let uvi = response2.current.uvi;
+      //transfer to HTML
+      $("#uv-index").text("UV Index: " + uvi);
+    });
   });
 });
