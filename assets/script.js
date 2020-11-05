9cfa40ebf758fa6097eeb9e9106b1b0000adc764
Peter Phenow <peter.phenow@gmail.com>
2020-11-04 22:11:31 -0600

    current weather data API and variables set up

--- assets/script.js
+++ assets/script.js
@@ -8,22 +8,32 @@ let citySearch = "";
 searchBtn.on("click", function (event) {
   event.preventDefault();
   citySearch = $("#search-input").val().trim();
-  return citySearch;
+
+  // Here we are building the URL we need to query the database
+  let queryURL1 =
+    "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIKey;
+
+  // We then created an AJAX call
+  $.ajax({
+    url: queryURL1,
+    method: "GET",
+  }).then(function (response) {
+    //log the resulting object
+    console.log(response);
+    //create lat and lon  and icon variables
+    let lat = response.coord.lat;
+    let lon = response.coord.lon;
+    let weatherIcon = response.weather[0].icon;
+    //create temperature variable and convert to fahrenheit
+    let tempF = ((response.main.temp - 273.15) * 1.8 + 32).toFixed(1);
+    //create humidity and wind variables
+    let humidity = response.main.humidity;
+    let wind = response.wind.speed;
+    console.log(lat, lon, weatherIcon, tempF, humidity, wind);
+
+    // Create CODE HERE to transfer content to HTML
+    $("#temp").text("Temperature: " + tempF);
+    $("#humidity").text("Humidity: " + humidity + "%");
+    $("#wind-speed").text("Wind Speed: " + wind + " mph");
+  });
 });
-
-// Here we are building the URL we need to query the database
-// let queryURL =
-//   "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
-
-// // We then created an AJAX call
-// $.ajax({
-//   url: queryURL,
-//   method: "GET",
-// }).then(function (response) {
-//   // Create CODE HERE to Log the queryURL
-//   // Create CODE HERE to log the resulting object
-//   // Create CODE HERE to calculate the temperature (converted from Kelvin)
-//   // Create CODE HERE to transfer content to HTML
-//   // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-//   // Create CODE HERE to dump the temperature content into HTML
-// });
